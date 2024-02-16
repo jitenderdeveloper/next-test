90adeda1d3d0cefb753898da52590c34c189d0d7
jitenderdeveloper <jitenderdeveloper0741@gmail.com>
2024-02-16 10:24:52 +0530

    updated

--- next.config.js
+++ next.config.js
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  //   css: ["@/app/globals.css"],
-  //   css: ["@/app/custom.css"],
-  swcMinify: true,
-  experimental: {
-    images: {
-      upoptimized: true,
-    },
-  },
+  css: ["@/app/globals.css"],
+  css: ["@/app/custom.css"],
+  swcMinify: false,
+  // experimental: {
+  //   images: {
+  //     upoptimized: true,
+  //   },
+  // },
   trailingSlash: true,
 };
 
