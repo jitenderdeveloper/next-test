252b4d0ae1bc81b693c1d20b49ea506bea104c79
jitenderdeveloper <jitenderdeveloper0741@gmail.com>
2024-02-16 18:04:17 +0530

    updated

--- src/app/user-profile/page.js
+++ src/app/user-profile/page.js
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import MyProfile from "../components/myProfile/MyProfile";
 
